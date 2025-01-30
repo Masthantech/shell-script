d20cb1b831bdddd12ab09da1dcafde9de6783063
masthantech <masthantech619@gnail.com>
2025-01-30 09:45:28 +0530

    changes done

--- 02-talk.sh
+++ 02-talk.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-echo "Aman: Hi Amina, how are you"
-echo "Amina: Hello Aman, I am fine"
-echo "Aman: How is you day!"
-echo "Amina: It was a great day!"
+
+PERSON1=Aman #no space before and after equal
+PERSON2=Amina
+echo "$PERSON1:: Hi $PERSON2, how are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine"
+echo "$PERSON1:: How is you day?"
+echo "$PERSON2:: It was a great day!"
 
 
