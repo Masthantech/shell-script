bec5faad7d6c9d3f24577309d37fb0f1f2b901ec
masthantech <masthantech619@gnail.com>
2025-01-30 14:46:40 +0530

    accounts script updated

--- 04-accouts.sh
+++ 04-accouts.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "Please enter your username::" #text entered here will be added as value to the variable
-read USERNAME # here USERNAME is variable
+read -s USERNAME # here USERNAME is variable
 echo "username is ::$USERNAME"
+echo "enter your password::"
+read -s PASSWORD
+echo "welcome to your machine"
 
