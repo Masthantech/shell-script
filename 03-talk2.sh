13407e60f43406e3c12a23c517c898e1833666c8
masthantech <masthantech619@gnail.com>
2025-01-30 10:36:19 +0530

    new talk added

---
+++ 03-talk2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1 #no space before and after equal
+PERSON2=$2
+echo "$PERSON1:: Hi $PERSON2, how are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine"
+echo "$PERSON1:: How is you day?"
+echo "$PERSON2:: It was a great day!"
