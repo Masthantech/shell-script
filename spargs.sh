adb59156a9976c91ea7cb5604d0c1dab888df0b6
masthantech <masthantech619@gnail.com>
2025-01-31 06:28:21 +0530

    spargs script added

---
+++ spargs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#I am writing special args
+
+echo "Script name is: $0"
+echo "First argument is: $1"
+echo "Second argument is: $2"
+echo "Total nuber of arguments are: $#"
+echo "All arguments as list: $@"
+echo "All argumets as a string: $*"
+
