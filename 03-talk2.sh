f9e845386961f6de683e782c1e3e000bffe06102
khizar <masthantech619@gmail.com>
2025-04-06 11:28:12 +0530

    03-talk updated

--- 03-talk2.sh
+++ 03-talk2.sh
@@ -3,6 +3,11 @@
 
 echo "Please enter your name: "
 
-read USERNAME 
+read  USERNAME 
  echo "username entered is: $USERNAME "
- 
+
+ echo "Please enter your password: "
+
+ read PASSWORD 
+
+ echo "Your successfully logged in"
