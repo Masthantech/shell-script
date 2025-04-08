8112e950b178816679407bab1f50e5901429c429
khizar <masthantech619@gmail.com>
2025-04-08 14:41:33 +0530

    install script updated

--- install.sh
+++ install.sh
@@ -2,5 +2,12 @@
 
 # writing the script to install the mysql
 
-dnf install mysql -y
+USERID=$(id -u)
 
+if [$USERID -ne 0]
+then 
+     echo "you need root permissions to run this script"
+else 
+    dnf install mysql -y
+
+fi
