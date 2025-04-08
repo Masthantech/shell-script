08855f8e64f600e100569e203227a4a59dd6cc93
khizar <masthantech619@gmail.com>
2025-04-08 15:15:52 +0530

    install script updated

--- install.sh
+++ install.sh
@@ -7,9 +7,24 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
      echo "you need root permissions to run this script"
-     exit 1
+     exit 1      
 fi 
 
-dnf install mysqll  -y
+dnf list installed git 
 
-dnf install git -y 
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ] 
+    then
+        echo "git installation is failure"
+        exit 1
+    else 
+        echo "git installtion in sucess"
+    fi
+
+else 
+    echo "git is already installed"  
+fi              
+    
+    
