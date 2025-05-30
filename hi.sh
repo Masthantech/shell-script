d970dda652e279cfb402ca7a33dd90176c7a737b
khizar <masthantech619@gmail.com>
2025-05-30 14:49:03 +0530

    hi.sh script added

---
+++ hi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hi, i resume my devops journey. I am starting shell scripting . This time I won't step back or stop my learning journey 
+in between. I will complete it properly"
+
