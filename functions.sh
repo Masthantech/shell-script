eb1eeb65cf5d9dc26c915b660acec2353020f2cc
khizar <masthantech619@gmail.com>
2025-05-31 19:35:11 +0530

    functions script added

---
+++ functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ] ; then
+echo "You need sudo access to run the script"
+
+validate () {
+   if [ $1 -ne 0 ] ; then
+        echo "Git installation.....Failure"
+        exit 1
+    else 
+        echo "Git installtion......Success"
+    fi  
+}
+
+dnf list installed git 
+if [ $? -ne 0 ] ; then 
+    dnf install git -y 
+    validate () $?
+else 
+    echo "Git already installed on machine"  
+
+fi 
+
+
