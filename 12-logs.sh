6ed1ab84f53c924fa06aecce40bdd0fcabfccc95
khizar <masthantech619@gmail.com>
2025-06-05 12:44:58 +0530

    logs script updated

--- 12-logs.sh
+++ 12-logs.sh
@@ -6,26 +6,32 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+FOLDER_NAME=${"/var/log/shellscript-logs"}
+FILE_NAME= $(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$FOLDER_NAME/$FILE_NAME-$TIMESTAMP.log"
 
 validate() {
     if [ $1 -ne 0 ]; then 
-       echo -e "$2  $R...Failure $N"
+       echo -e "$2  $R FAILURE $N"
        exit 1
     else 
-        echo -e "$2  $G....Success $N"
+        echo -e "$2  $G SUCCESS $N"
     fi       
 }
+
+echo "script started executing at:: $TIMESTAMP" &>> $LOG_FILE_NAME
 
 if [ ${USERID} -ne 0 ]; then 
    echo "You need sudo access to execute this script"
    exit 1
 fi 
 
-dnf list installed git 
+dnf list installed git &>> $LOG_FILE_NAME
 
-if [ $! -ne 0 ]; then 
-    validate $! "Git installtion" 
+if [ $? -ne 0 ]; then 
+    dnf install git -y
+    validate $? "GIT INSTALLATION..." 
 else 
-    echo -e "Git already  $Y...Installed $N"
+    echo -e "Git Already...  $Y INSTALLED $N"
 fi        
