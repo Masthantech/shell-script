2c4ac7ba189ec60199650dbd44f62aafd544c445
khizar <masthantech619@gmail.com>
2025-06-08 10:53:11 +0530

    loops2 script added

---
+++ loops.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+echo "Hi"
+echo "Hello"
+echo "Howru"
+
+greeting=("Hi" "Hello" "Howru" "Finetk") 
+
+for i in {0..4} 
+do 
+echo "$i"
+done
+
---
+++ loops2.sh
@@ -0,0 +1,62 @@
+#!/binbash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[32m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$( echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+validate() {
+    if ( $1 -ne 0 ); then 
+        echo -e "$2 $R FAILURE $N"
+        exit 1 
+    else 
+        echo -e "$2  $G SUCCESS $N"
+    fi        
+
+}
+
+echo "The script started running at: $TIMESTAMP" &>> $LOF_FILE_NAME
+
+
+if ( $? -ne 0 ); then 
+
+    echo "ERROR:: You need sudo access to execute the script"
+    exit 1
+fi   
+
+
+for package in $@
+do
+dnf list installed $package &>> $LOF_FILE_NAME
+
+if ( $? -ne 0 ); then 
+    dnf install $package -y &>> $LOF_FILE_NAME
+    if ( $? -ne 0 ); then
+        validate $? "$package Installation..."
+    else 
+        echo -e "$package Already.... $Y INSTALLED $N"  
+fi  
+
+done
+
+
+# dnf list installed git &>> $LOF_FILE_NAME
+
+# if ( $? -ne 0 ); then 
+#     dnf install git -y &>> $LOF_FILE_NAME
+#     if ( $? -ne 0 ); then
+#         validate $? "Git Installation..."
+#     else 
+#         echo "Git Already.... $Y INSTALLED $N"  
+# fi  
+
+
+
