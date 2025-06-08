499799c60b2a10961232d4b9870d53047e8104e7
khizar <masthantech619@gmail.com>
2025-06-08 11:38:35 +0530

    loops2 script updated

--- loops2.sh
+++ loops2.sh
@@ -1,49 +1,93 @@
 #!/binbash
 
 USERID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
-Y="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
-LOG_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$( echo $0 | cut -d "." -f1 )
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-
-validate() {
-    if [ $1 -ne 0 ]; then 
-        echo -e "$2 $R FAILURE $N"
-        exit 1 
-    else 
-        echo -e "$2  $G SUCCESS $N"
-    fi        
-
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
-echo "The script started running at: $TIMESTAMP" &>> $LOG_FILE_NAME
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1 #other than 0
+    fi
+}
 
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-if [ $USERID -ne 0 ]; then 
-    echo "ERROR:: You need sudo access to execute the script"
-    exit 1
-fi   
-
+CHECK_ROOT
 
 for package in $@
 do
-    dnf list installed $package &>> $LOG_FILE_NAME
-
-    if [ $? -ne 0 ]; then 
-        dnf install $package -y &>> $LOG_FILE_NAME
-        validate $? "$package Installation..."
-    else 
-        echo -e "$package Already.... $Y INSTALLED $N"  
-    fi  
-
+    dnf list installed $package &>>$LOG_FILE_NAME
+    if [ $? -ne 0 ]
+    then
+        dnf install $package -y &>>$LOG_FILE_NAME
+        VALIDATE $? "Installing $package"
+    else
+        echo -e "$package is already $Y ... INSTALLED $N"
+    fi
 done
+# USERID=$(id -u)
+
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[32m"
+# N="\e[0m"
+
+# LOG_FOLDER="/var/log/shellscript-logs"
+# LOG_FILE=$( echo $0 | cut -d "." -f1 )
+# TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+# LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+# validate() {
+#     if [ $1 -ne 0 ]; then 
+#         echo -e "$2 $R FAILURE $N"
+#         exit 1 
+#     else 
+#         echo -e "$2  $G SUCCESS $N"
+#     fi        
+
+# }
+
+# echo "The script started running at: $TIMESTAMP" &>> $LOG_FILE_NAME
+
+
+# if [ $USERID -ne 0 ]; then 
+#     echo "ERROR:: You need sudo access to execute the script"
+#     exit 1
+# fi   
+
+
+# for package in $@
+# do
+#     dnf list installed $package &>> $LOG_FILE_NAME
+
+#     if [ $? -ne 0 ]; then 
+#         dnf install $package -y &>> $LOG_FILE_NAME
+#         validate $? "$package Installation..."
+#     else 
+#         echo -e "$package Already.... $Y INSTALLED $N"  
+#     fi  
+
+# done
 
 
 # dnf list installed git &>> $LOF_FILE_NAME
