f61692cd49029f3a72f4ec2ac0ae1009febe7741
khizar <masthantech619@gmail.com>
2025-06-08 11:35:28 +0530

    loops2 script updated

--- loops2.sh
+++ loops2.sh
@@ -14,7 +14,7 @@ LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 
 validate() {
-    if ( $1 -ne 0 ); then 
+    if [ $1 -ne 0 ]; then 
         echo -e "$2 $R FAILURE $N"
         exit 1 
     else 
@@ -26,8 +26,7 @@ validate() {
 echo "The script started running at: $TIMESTAMP" &>> $LOG_FILE_NAME
 
 
-if ( $USERID -ne 0 ); then 
-
+if [ $USERID -ne 0 ]; then 
     echo "ERROR:: You need sudo access to execute the script"
     exit 1
 fi   
@@ -37,7 +36,7 @@ for package in $@
 do
     dnf list installed $package &>> $LOG_FILE_NAME
 
-    if ( $? -ne 0 ); then 
+    if [ $? -ne 0 ]; then 
         dnf install $package -y &>> $LOG_FILE_NAME
         validate $? "$package Installation..."
     else 
