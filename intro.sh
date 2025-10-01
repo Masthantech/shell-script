3df553e90f39f698734766faa6a399c2547902ad
khizar <masthantech619@gmail.com>
2025-10-01 15:16:57 +0530

    intro script updated

--- intro.sh
+++ intro.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 echo "Hello world!"
+
+a=1
+b=2 
+#addition
+sum=$((a+b))
+
+echo "The sum of $a and $b is: $sum"
+#taking inputs as args 
+
+a=$1
+b=$2 
+
+multi=$((a*b))
+
+echo "the value of multi $a and $b is: $multi"
+
