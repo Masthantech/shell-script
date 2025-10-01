181bcdd18577fa0a60b1122a63f90d7b5412cd8e
khizar <masthantech619@gmail.com>
2025-10-01 15:44:38 +0530

    script3 added

---
+++ script3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#writing the script to read the user name and password from user and granting him access
+
+echo "Please enter you name #vibe check: "
+read Name 
+
+echo "$Name, please enter your un-professional passcode: "
+
+read -s password 
+
+echo "#enjoy Pandagooo"
--- talk.sh
+++ talk.sh
@@ -5,6 +5,6 @@ person2=$2
 
 echo "$person1: Hi $person2, kya haal chaal"
 echo "$person2: Hello $person1 ji, main theek hu"
-echo "R$person1: Iss dewali ko hame kya gift derahe ho"
+echo "$person1: Iss dewali ko hame kya gift derahe ho"
 echo "$person2: Iss tyohaar, GST ka chutkaar. Just kidding! sirf thoda GST klam kar raha hu"
 
