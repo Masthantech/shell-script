a126d449e99e99e7b378151b93cf6b01b04cc368
khizar <masthantech619@gmail.com>
2025-10-01 15:30:12 +0530

    talk script added

---
+++ talk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+echo "Rahul: Hi Modiji, kya haal chaal"
+echo "Modiji: Hello Rahul ji, main theek hu"
+echo "Rahul: Iss dewali ko hame kya gift derahe ho"
+echo "Modiji: Iss tyohaar, GST ka chutkaar. Just kidding! sirf thoda GST klam kar raha hu"
+
